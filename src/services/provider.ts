68e193fba10fa8d163778010636f22c93d115d51
Alan Jenkins <alan@functionfirst.co.uk>
2022-02-06 21:23:29 +0000

    Update provider.ts

--- src/services/provider.ts
+++ src/services/provider.ts
@@ -1,9 +1,7 @@
 import { ILoanData } from '@/entities'
 import { LoansService, IProvider } from '@/services'
-// import { LoanStore } from '@/store'
 
 export const provider = (data: ILoanData[]): IProvider => {
-  // const store = new LoanStore()
   return {
     loans: new LoansService(data)
   }
