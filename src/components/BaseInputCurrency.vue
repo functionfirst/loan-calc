e3125492603379e3ec56bf21dbac637c91609d5b
Alan Jenkins <alan@functionfirst.co.uk>
2022-02-06 14:48:41 +0000

    Update BaseInputCurrency.vue

--- src/components/BaseInputCurrency.vue
+++ src/components/BaseInputCurrency.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="flex items-stretch">
     <CurrencyDropdown
-      :value="currency"
       class="border border-r-0 border-gray-300 rounded-l"
       v-bind="{ required }"
+      :value="currency"
       @input="(event) => $emit('update:currency', event.target.value)"
     />
     <BaseInput
-      v-bind="{ id, placeholder, required }"
       class="flex-1 rounded-l-none"
+      v-bind="{ id, placeholder, required }"
       type="number"
       :value="amount"
       @input="(event) => $emit('update:amount', event.target.value)"
